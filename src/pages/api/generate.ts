7db561c089e61cbbbaa7fe816b3bb19a91f58a58
p-charon <1209727216@qq.com>
2024-10-07 14:34:02 +0800

    generate.ts

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -43,7 +43,8 @@ export const post: APIRoute = async(context) => {
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error
-  const response = await fetch(`${baseUrl}/v1/chat/completions`, initOptions).catch((err: Error) => {
+  //const response = await fetch(`${baseUrl}/v1/chat/completions`, initOptions).catch((err: Error) => {
+  const response = await fetch(`${baseUrl}`, initOptions).catch((err: Error) => {
     console.error(err)
     return new Response(JSON.stringify({
       error: {
